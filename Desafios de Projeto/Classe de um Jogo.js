874a20c7d58e5951751964d13397a2e8b4fe4db4
Frederico <sanderfn@hotmail.com>
2023-11-02 16:12:08 -0300

    Atualização de arquivos

---
+++ Desafios de Projeto/Classe de um Jogo.js
@@ -0,0 +1,37 @@
+
+class Heroi{
+    constructor( nome, idade, tipo){
+        this.nome = nome 
+        this.idade = idade
+        this.tipo = tipo
+        }
+    
+        atacar(){
+            let ataque = "";
+            switch (this.tipo){
+                case "mago":
+                    ataque = " usou magia";
+                    break;
+                case "guerreiro":
+                    ataque = "usou espada";
+                    break;
+                case "monge":
+                    ataque = "usou artes marciais";
+                    break;
+                case "ninja":
+                    ataque = "usou shuriken";
+                    break;
+                default:
+                    ataque = "usou ataque desconhecido";
+            }
+
+            console.log(`O ${this.tipo} atacou usando ${ataque} `)
+            }
+}
+
+let heroi = new Heroi("heroi",25,"ninja");
+heroi.atacar();
+
+let heroi2 = new Heroi("heroi2",40,"monge");
+heroi2.atacar();
+
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Programação do Zero Potencia Tech 2023
+# Programação do Zero Potência Tech 2023
 Este repositório será utilizado para armazenamento das atividades desenvolvidas no Bootcamp Programação do Zero. O repositório é dividido conformes os médulos estudados no Bootcamp.
    
 - [Lógica de Programação](): em construção
 - [Explorando Operadores](): em construção
 - [Estruturas de controle](): em construção
 - [Funções](https://github.com/Sanderfn/Programacao_do_Zero_Potencia_Tech_2023/tree/main/Funcoes): em construção.
-- [Dados e Objetos](): em construção
+- [Dados e Objetos](https://github.com/Sanderfn/Programacao_do_Zero_Potencia_Tech_2023/tree/main/Dados%20e%20Objetos): em construção
 - [Desafios de Projeto](https://github.com/Sanderfn/Programacao_do_Zero_Potencia_Tech_2023/tree/main/Desafios%20de%20Projeto): Na pasta desafios de projetos estão armazenados o arquivos referentes aos projetos desenvolvidos durante a jornada.
